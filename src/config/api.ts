0cb044ebfeb34339df71ba88828f110e2a0c079e
TimurBatrshin <65765228+TimurBatrshin@users.noreply.github.com>
2025-04-23 00:46:24 +0300

    v1.7.2

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,5 @@
 export const API_CONFIG = {
   baseUrl: 'https://timurbatrshin-fiba-backend-1aa7.twc1.net/api',
-  mockUrl: 'https://fiba-mock.vercel.app/api',
   localUrl: 'http://localhost:8080',
   fallbackUrl: 'https://fiba-mock.vercel.app/api',
   useMockByDefault: true,
